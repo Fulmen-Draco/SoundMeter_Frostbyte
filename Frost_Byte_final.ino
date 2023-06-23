6b70a3428b0dd4749eed65107c827858abd9685f
Fulmen-Draco <92170969+Fulmen-Draco@users.noreply.github.com>
2023-06-23 12:26:57 +0530

    Add files via upload

---
+++ Frost_Byte_final.ino
@@ -0,0 +1,37 @@
+/* arduino sound meter developed by Frostbyte*/
+const int sampleWindow = 50;
+unsigned int sample;
+void setup() {
+Serial.begin(9600);
+}
+void loop() {
+unsigned long startMillis= millis();
+unsigned int peaktopeak= 0;
+
+unsigned int signalMax = 0;
+unsigned int signalMin = 1024;
+while (millis() - startMillis < sampleWindow){
+sample = analogRead(0);
+if(sample < 1024)
+{
+if (sample > signalMax)
+{
+signalMax = sample;
+}
+else if (sample < signalMin)
+{
+signalMin = sample;
+
+}
+}
+}
+peaktopeak = signalMax - signalMin;
+double volts = ((peaktopeak * 3.3)/ 1024) * 0.707;
+double firstvalue = log10(volts/0.00631)*20;
+double secondvalue = firstvalue + 94 - 44 -25;
+Serial.println(secondvalue);
+
+
+}
+
+
---
+++ frost_byte.ino
@@ -0,0 +1,33 @@
+const int sampleWindow = 50;
+unsigned int sample;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long startMillis= millis();
+  float peaktopeak= 0;
+  
+  unsigned int signalMax = 1024;
+  unsigned int signelMin = 0;
+
+  while (millis() - startMillis < sampleWindow){
+    sample = analogRead(0);
+    if(sample < 1024)
+    {
+      if (sample > signalMax)
+      {
+        signalMin = sample; 
+      }
+      else if (sample < signalMin)
+      {
+       signalMin = sample; 
+        
+      }
+    }
+  }
+  peaktopeak = signalMax - signalMin;
+  float db = map(peaktopeak,20,900,49.5,90);
+  print
+}
+
